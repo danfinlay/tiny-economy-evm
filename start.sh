7f051193806dcbe6f3fdb3dc3398cb555adfba59
Dan Finlay <dan@danfinlay.com>
2019-07-17 16:50:47 -0700

    First commit

---
+++ start.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# --db persists & restores between boots
+# -h selects host to bind to
+# -p selects port to bind to
+# -n locks all accounts by default (secure mode)
+ganache-cli --db ./db -h dankoin.local -p 6510 -n
+
