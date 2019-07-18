864222095e842b51f335dd66d1cf1811f3b893d7
Dan Finlay <542863+danfinlay@users.noreply.github.com>
2019-07-18 10:50:23 -0700

    Parameterized arguments

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@
 # -h selects host to bind to
 # -p selects port to bind to
 # -n locks all accounts by default (secure mode)
-ganache-cli --db ./db -h dankoin.local -p 6510 -n
+ganache-cli --db ./db -h $1 -p $2 -n
 
